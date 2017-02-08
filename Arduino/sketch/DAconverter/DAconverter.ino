901e89975949243bd2aba57a8859a7762429aa1a
yusatakeshi <yusa.takeshi@gmail.com>
2017-02-08 16:30:43 +0900

    Arduino: DAconverterのコードリファクタリング

--- Arduino/sketch/DAconverter/DAconverter.ino
+++ Arduino/sketch/DAconverter/DAconverter.ino
@@ -25,6 +25,7 @@ http://brainwagon.org/2011/02/24/arduino-mcp4725-breakout-board/
 
 //This is the I2C Address of the MCP4725, by default (A0 pulled to GND).
 //Please note that this breakout is for the MCP4725A0. 
+//For devices with A0 pulled HIGH, use 0x61
 #define MCP4725_ADDR1 0x60
 #define MCP4725_ADDR2 0x61 // A0 pin pulled up
 
@@ -36,15 +37,27 @@ http://brainwagon.org/2011/02/24/arduino-mcp4725-breakout-board/
 #define PWM_VAL_MIN            1000  //PWM input value min
 #define PULSEIN_RETVAL_TIMEOUT 0     //pulseIn return value when it is timeout
 #define PULSEIN_TIMEOUT        25000 //pulseIn timeout [us]
-#define ONEBYTE_OFFSET         4     //offset bitnumber
+#define BIT_OFFSET             4     //offset bitnumber:because output is 12bit but I2C communication is done in each 8bit 
 #define DA_OUTVAL_MAX          4095  //MCP4725 I2C output value max
 #define DA_OUTVAL_MID          2047  //MCP4725 I2C output value middle
 #define DA_OUTVAL_MIN          0     //MCP4725 I2C output value min
+//Config Bits
+//Write Option
+#define FAST_WRITE                      0x00  //FastMode Write
+#define NORMAL_WRITE_TO_REG             0x40  //NormalMode Write to Register
+#define NORMAL_WRITE_TO_REG_AND_EEPROM  0x60  //NormalMode Write to Register and EEPROM
+//Power Down Selection
+#define PD_MODE_NORMAL                  0x00  //PowerDown Mode option(1of4):Analog output pin is open
+#define PD_MODE_1KOHM                   0x02  //PowerDown Mode option(2of4):Analog output pin is pulled down by 1kohm resistor
+#define PD_MODE_100KOHM                 0x04  //PowerDown Mode option(3of4):Analog output pin is pulled down by 100kohm resistor
+#define PD_MODE_500KOHM                 0x06  //PowerDown Mode option(4of4):Analog output pin is pulled down by 500kohm resistor
+#define FASTMODE_PD_BIT_OFFSET          3     //PowerDown Option Bit offsets when it uses FastMode Write command.
+
 
 int ch1; // Here's where we'll keep our channel values
 int ch2;
 
-//For devices with A0 pulled HIGH, use 0x61
+
 
 //Sinewave Tables were generated using this calculator:
 //http://www.daycounter.com/Calculators/Sine-Generator-Calculator.phtml
@@ -82,22 +95,22 @@ void loop()
   }
   // output I2C to MCP4725s
   Wire.beginTransmission(MCP4725_ADDR1);
-  Wire.write(64);                                  // cmd to update the DAC
-  Wire.write(val1 >> ONEBYTE_OFFSET);              // the 8 most significant bits...
-  Wire.write((val1 & 0x0F) << ONEBYTE_OFFSET);     // the 4 least significant bits...
+  Wire.write(NORMAL_WRITE_TO_REG & PD_MODE_NORMAL);  // cmd to update the DAC
+  Wire.write(val1 >> BIT_OFFSET);               // the 8 most significant bits...
+  Wire.write((val1 & 0x0F) << BIT_OFFSET);      // the 4 least significant bits...
   Wire.endTransmission();
   
   Wire.beginTransmission(MCP4725_ADDR2);
-  Wire.write(64);                                  // cmd to update the DAC
-  Wire.write(val2 >> ONEBYTE_OFFSET);              // the 8 most significant bits...
-  Wire.write((val2 & 0x0F) << ONEBYTE_OFFSET);     // the 4 least significant bits...
+  Wire.write(NORMAL_WRITE_TO_REG & PD_MODE_NORMAL);  // cmd to update the DAC
+  Wire.write(val2 >> BIT_OFFSET);               // the 8 most significant bits...
+  Wire.write((val2 & 0x0F) << BIT_OFFSET);      // the 4 least significant bits...
   Wire.endTransmission();
 }
 
 //range convert from PWM pulse width to MCP4725 12bit value 
 int cnvrng(int _pwm)
 {
-  int out pwm;
+  int out, pwm;
   
   //constrain PWM value
   if(_pwm < PWM_VAL_MIN){
